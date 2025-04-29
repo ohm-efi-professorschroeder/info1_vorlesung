a7b5f4d0998d9c765ae1a3b77c1e430372968bd4
enricoschroeder <10079627+enricoschroeder@users.noreply.github.com>
2025-04-29 15:20:28 +0200

    Stand aus der Vorlesung vom 29.4.

--- termin_05/01_maexchen.c
+++ termin_05/01_maexchen.c
@@ -27,12 +27,77 @@
  ***********************************************************************************************************************************************/
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// TODO
+#define MIN(x,y) ((x) < (y) ? (x) : (y))
+#define MAX(x,y) ((x) < (y) ? (y) : (x))
+
+int wuerfeln(int wuerfelseiten);
+int berechnePunkte(int z1, int z2);
 
 int main()
 {
-    // TODO
+    srand(time(0));
+    // solange neuePunkte > altePunkte
+        // 1. wuerfeln -> Funktion
+        // 2. punkte berechnen -> Funktion
+        // 3. punkte vergleichen -> keine Funktion
+        // 4. gewinner/verlierer bestimmen -> keine Funktion
+    int altePunkte = -1, neuePunkte = 0;
+    unsigned int runde = 0;
+
+    do {
+        altePunkte = neuePunkte;
+        int z1 = wuerfeln(6);
+        int z2 = wuerfeln(6);
+        neuePunkte = berechnePunkte(z1, z2);
+        runde++;
+
+        if(runde % 2 == 1)
+            printf("Der Computer wuerfelt %d\n", neuePunkte);
+        else
+            printf("Der Spieler wuerfelt %d\n", neuePunkte);
+
+    }
+    while(altePunkte < neuePunkte);
+
+    if(runde % 2 == 0)
+        printf("Der Computer hat gewonnen!\n");
+    else
+        printf("Der Spieler hat gewonnen!\n");
+
 
     return 0;
+}
+
+// Funktion: <Rueckgabetyp> <Namen> (<Parameter1>, <Parameter2>,...)
+int wuerfeln(int wuerfelseiten)
+{
+    int zahl = rand() % wuerfelseiten + 1;
+    return zahl;
+}
+
+int berechnePunkte(int z1, int z2)
+{
+    int punkte = 0;
+    // Variante 1: if
+    // if(z1 < z2)
+    //     punkte = z2 * 10 + z1;
+    // else 
+    //     punkte = z1 * 10 + z2;
+    // Variante 2: Makrofunktion
+    int zMin = MIN(z1,z2); // (z1 < z2 ? z1 : z2)
+    int zMax = MAX(z1,z2);
+    punkte = zMax * 10 + zMin;
+    
+    // Pasch
+    if(z1 == z2)
+        punkte += 100;
+
+    // 21
+    if(punkte == 21)
+        punkte += 1000;
+
+    return punkte;
 }
